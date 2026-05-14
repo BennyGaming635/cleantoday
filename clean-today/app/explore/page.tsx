a034307ed77cc44aa223de1e9b172947ac0805ef
Benjamin Graetz <benjamin.graetz@henleyhs.sa.edu.au>
2026-05-14 19:13:45 +0930

    add user to explore page

--- clean-today/app/explore/page.tsx
+++ clean-today/app/explore/page.tsx
@@ -1,21 +1,21 @@
 'use client'
 
 import dynamic from 'next/dynamic'
-import Navbar from "@/components/navbar/Navbar";
+import Navbar from '@/components/navbar/Navbar'
 
 const CleanupMap = dynamic(
-    () => import('@/components/map/CleanupMap'),
-    { ssr: false }
+  () => import('@/components/map/CleanupMap'),
+  { ssr: false }
 )
 
 export default function ExplorePage() {
-    return (
-        <main className="h-screen w-screen overflow-hidden">
-            <Navbar />
+  return (
+    <main className="h-screen w-screen overflow-hidden">
+      <Navbar />
 
-            <div className="h-[calc(100vh-73px)] w-full">
-                <CleanupMap />
-            </div>
-        </main>
-    )
+      <div className="h-[calc(100vh-73px)] w-full">
+        <CleanupMap />
+      </div>
+    </main>
+  )
 }
