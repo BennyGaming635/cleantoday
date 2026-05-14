ebea86806ac1b613713c332892576510238a54a9
Benjamin Graetz <benjamin.graetz@henleyhs.sa.edu.au>
2026-05-14 18:43:22 +0930

    add icons for events

--- clean-today/components/map/CleanupMap.tsx
+++ clean-today/components/map/CleanupMap.tsx
@@ -1,18 +1,36 @@
 'use client'
 
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import {
+    MapContainer,
+    TileLayer,
+    Marker,
+    Popup,
+} from 'react-leaflet'
+
+import L from 'leaflet'
+import 'leaflet/dist/leaflet.css';
+delete (L.Icon.Default.prototype as unknown as Record<string, unknown>)._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl:
+        'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
+    iconUrl:
+        'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
+    shadowUrl:
+        'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
+})
 
 export default function CleanupMap() {
     return (
         <MapContainer center={[-34.9285, 138.6007]} zoom={11} className="h-full w-full">
             <TileLayer
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
 
             <Marker position={[-34.9285, 138.6007]}>
                 <Popup>
-                    Example Cleanup Event
+                    A cleanup event here!
                 </Popup>
             </Marker>
         </MapContainer>
