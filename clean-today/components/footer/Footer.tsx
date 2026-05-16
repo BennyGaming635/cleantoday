a5de57e624fdd534475b068af8b30db3178e1e0a
Benjamin Graetz <benjamin.graetz@henleyhs.sa.edu.au>
2026-05-16 18:17:39 +0930

    add gov login to footer and more ads

--- clean-today/components/footer/Footer.tsx
+++ clean-today/components/footer/Footer.tsx
@@ -34,7 +34,8 @@ export default function Footer() {
                             Government
                         </h3>
                         <div className="flex flex-col gap-2 text-sm text-gray-600">
-                            <Link href="/gov">Government Enquires</Link>
+                            <Link href="/gov">Gov Enquires</Link>
+                            <Link href="/gov/login">Gov Login</Link>
                         </div>
                     </div>
                     <div>
