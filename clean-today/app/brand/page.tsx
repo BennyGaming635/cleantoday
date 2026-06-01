cb4bc6b25353931a7b9d7a4d0a459287ebbab204
Benjamin Graetz <benjamin.graetz@henleyhs.sa.edu.au>
2026-06-01 21:19:57 +0930

    added names in colour

--- clean-today/app/brand/page.tsx
+++ clean-today/app/brand/page.tsx
@@ -61,6 +61,20 @@ export default function BrandPage() {
                 className="w-full max-h-40 object-contain bg-black p-6 rounded-lg"
               />
             </BrandCard>
+
+            <BrandCard title="Clean Today Name (Primary)">
+              <img
+                src="/brand/name-c.png"
+                className="w-full max-h-40 object-contain"
+              />
+            </BrandCard>
+
+            <BrandCard title="Clean Today Name (Secondary)">
+              <img
+                src="/brand/name-ci.png"
+                className="w-full max-h-40 object-contain p-6 rounded-lg bg-gradient-to-br from-[#169f48] to-[#14532d]"
+              />
+            </BrandCard>
           </div>
         </section>
 
