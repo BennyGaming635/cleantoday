1ce047843c9e3f0a85561c82235a99888c2ad9d8
Benjamin Graetz <benjamin.graetz@henleyhs.sa.edu.au>
2026-06-11 21:06:36 +0930

    add const for posts

---
+++ clean-today/app/blog/page.tsx
@@ -0,0 +1,10 @@
+import Navbar from '@/components/navbar/Navbar'
+import Link from 'next/link'
+
+const posts = [
+    {
+        title: 'Welcome to Clean Today',
+        slug: 'welcome',
+        excerpt: 'What is Clean Today and how can you get involved?'
+    },
+]
